5d267260fd75bf076a7dfb5baf629d232a9792b3
Bhaskar Kumar <thisisofficialbk@gmail.com>
2022-01-30 20:03:07 +0530

    Create multiple Route

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const app = express();
-app.get("/", function(request, response){
-  response.send("<h1>Hello</h1>");
+// create home route
+app.get("/", function(req, res){
+  res.send("<h1>Hello</h1>");
 });
-
+// create a contact route
+app.get("/contact",function(req,res){
+  res.send("Contact me at bhaskarkumar.india@gmail.com")
+});
+// create aboute route
+app.get("/about",function(req,res){
+  res.send("Hii, I am Bhaskar Kumar.  I am currently learning ExpressJs. I hope i will master this skill soon.")
+})
+// Create a hobbie route
+app.get("/hobbie",function(req,res){
+  res.send("<ul><li>coffee</li><li>code</li></ul>")
+})
 app.listen(3000,function(){
   console.log("Server Started at port 3000");
 });
