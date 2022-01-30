f231bc603d1b26e64cf2912c589dfb427c45f463
Bhaskar Kumar <thisisofficialbk@gmail.com>
2022-01-30 17:04:08 +0530

    create GET request to send some data to browser

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+app.get("/", function(request, response){
+  response.send("Hello");
+});
+
 app.listen(3000,function(){
   console.log("Server Started at port 3000");
 });
